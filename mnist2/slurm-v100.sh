1f892bca5fa71ce4815ef5c8811bc0cc31eeca69
Gregor von Laszewski <laszewski@gmail.com>
2023-04-28 19:27:42 -0400

    working version in mnist1

--- mnist2/slurm-v100.sh
+++ mnist2/slurm-v100.sh
@@ -2,14 +2,17 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --time=12:00:00
+#SBATCH --time=01:00:00
 #SBATCH --partition=bii-gpu
 #SBATCH --account=bii_dsc_community
 #SBATCH --gres=gpu:v100:1
-#SBATCH --job-name=MYNAME
+#SBATCH --job-name=mnist-2
 #SBATCH --output=%u-%j.out
 #SBATCH --error=%u-%j.err
 
 nvidia-smi
+
 singularity exec --nv mnist.sif python mnist.py
+
+echo "==================================================="
 seff $SLURM_JOB_ID
