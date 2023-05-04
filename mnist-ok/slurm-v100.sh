5fb117b24005abe8986f96b18dca679461f7e1b0
Christopher Zelaya <chriszelaya01@outlook.com>
2023-05-04 17:13:18 -0400

    modified v100

--- mnist-ok/slurm-v100.sh
+++ mnist-ok/slurm-v100.sh
@@ -4,9 +4,9 @@
 #SBATCH --ntasks=1
 #SBATCH --time=01:00:00
 #SBATCH --partition=bii-gpu
-#SBATCH --account=bii_dsc_community
+#SBATCH --account=nssac_students
 #SBATCH --gres=gpu:v100:1
-#SBATCH --job-name=mnist-1
+#SBATCH --job-name=mnist-1-v100
 #SBATCH --output=%u-%j.out
 #SBATCH --error=%u-%j.err
 
